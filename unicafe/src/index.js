2b3b06213cd51e58dee456dfa9f14e7e41d8a98d
Jere Tofferi <jere.tofferi@gmail.com>
2018-05-09 00:55:12 +0300

    last save 9_5_2018

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>Hello world!</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
